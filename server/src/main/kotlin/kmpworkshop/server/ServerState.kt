e469cc756ff46f36a24be7e9296a6ec9ae4b9743
wout.werkman <wout.werkman@jetbrains.com>
2024-10-04 21:57:30 +0200

    Introduced ServerState

---
+++ server/src/main/kotlin/kmpworkshop/server/ServerState.kt
@@ -0,0 +1,26 @@
+package kmpworkshop.server
+
+import kmpworkshop.common.ApiKey
+import kmpworkshop.common.getEnvironment
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class Participant(val name: String, val apiKey: ApiKey)
+
+@Serializable
+internal data class ServerState(
+    val participants: List<Participant>,
+    val unverifiedParticipants: List<Participant>,
+)
+
+private val serverStateLock = Mutex()
+private var serverState by fileBackedProperty<ServerState>(getEnvironment()!!["server-database-file"]!!)
+
+internal suspend fun <T> updateServerStateAndGetValue(update: (ServerState) -> Pair<ServerState, T>): T =
+    serverStateLock.withLock {
+        val (newState, value) = update(serverState)
+        serverState = newState
+        value
+    }
