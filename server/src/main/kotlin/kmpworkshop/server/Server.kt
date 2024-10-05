24fa4615d9f6e84aff65e83f4d15acba481ef9c0
wout.werkman <wout.werkman@jetbrains.com>
2024-10-05 09:30:48 +0200

    Introduce ServerUI

--- server/src/main/kotlin/kmpworkshop/server/Server.kt
+++ server/src/main/kotlin/kmpworkshop/server/Server.kt
@@ -1,46 +1,67 @@
 package kmpworkshop.server
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import kmpworkshop.common.ApiKey
 import kmpworkshop.common.ApiKeyRegistrationResult
 import kmpworkshop.common.NameVerificationResult
 import kmpworkshop.common.WorkshopService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.*
+import kotlin.coroutines.CoroutineContext
 
 fun main(): Unit = runBlocking {
-    serveSingleService<WorkshopService> { coroutineContext ->
-        object: WorkshopService {
-            override suspend fun registerApiKeyFor(name: String): ApiKeyRegistrationResult =
-                updateServerStateAndGetValue { oldState ->
-                    when {
-                        !"[A-z 0-9]{1,20}".toRegex().matches(name) -> oldState to ApiKeyRegistrationResult.NameTooComplex
-                        oldState.participants.any { it.name == name } -> oldState to ApiKeyRegistrationResult.NameAlreadyExists
-                        else -> UUID.randomUUID().toString()
-                            .let { Participant(name, ApiKey(it)) }
-                            .let { oldState.copy(unverifiedParticipants = oldState.unverifiedParticipants + it) to ApiKeyRegistrationResult.Success(it.apiKey) }
-                    }
-                }
-
-            override suspend fun verifyRegistration(key: ApiKey): NameVerificationResult =
-                updateServerStateAndGetValue { oldState ->
-                    val name = oldState
-                        .unverifiedParticipants
-                        .firstOrNull { it.apiKey == key }
-                        ?.name
-                        ?: return@updateServerStateAndGetValue oldState to NameVerificationResult.ApiKeyDoesNotExist
-                    val stateWithoutUnverifiedParticipant = oldState.copy(
-                        unverifiedParticipants = oldState.unverifiedParticipants.filter { it.apiKey != key },
-                    )
-                    if (oldState.participants.any { it.name == name })
-                        return@updateServerStateAndGetValue stateWithoutUnverifiedParticipant to NameVerificationResult.NameAlreadyExists
-                    stateWithoutUnverifiedParticipant.copy(
-                        participants = stateWithoutUnverifiedParticipant.participants + Participant(name, key)
-                    ) to NameVerificationResult.Success
-                }
-
-            override val coroutineContext = coroutineContext
+    launch(Dispatchers.Default) {
+        serveSingleService<WorkshopService> { coroutineContext ->
+            workshopService(coroutineContext)
         }
     }
+    application {
+        Window(onCloseRequest = ::exitApplication, title = "KMP Workshop") {
+            MaterialTheme {
+                ServerUi()
+            }
+        }
+    }
+}
+
+private fun workshopService(coroutineContext: CoroutineContext): WorkshopService = object : WorkshopService {
+    override suspend fun registerApiKeyFor(name: String): ApiKeyRegistrationResult =
+        updateServerStateAndGetValue { oldState ->
+            when {
+                !"[A-z 0-9]{1,20}".toRegex().matches(name) -> oldState to ApiKeyRegistrationResult.NameTooComplex
+                oldState.participants.any { it.name == name } -> oldState to ApiKeyRegistrationResult.NameAlreadyExists
+                else -> UUID.randomUUID().toString()
+                    .let { Participant(name, ApiKey(it)) }
+                    .let {
+                        oldState.copy(unverifiedParticipants = oldState.unverifiedParticipants + it) to ApiKeyRegistrationResult.Success(
+                            it.apiKey
+                        )
+                    }
+            }
+        }
+
+    override suspend fun verifyRegistration(key: ApiKey): NameVerificationResult =
+        updateServerStateAndGetValue { oldState ->
+            val name = oldState
+                .unverifiedParticipants
+                .firstOrNull { it.apiKey == key }
+                ?.name
+                ?: return@updateServerStateAndGetValue oldState to NameVerificationResult.ApiKeyDoesNotExist
+            val stateWithoutUnverifiedParticipant = oldState.copy(
+                unverifiedParticipants = oldState.unverifiedParticipants.filter { it.apiKey != key },
+            )
+            if (oldState.participants.any { it.name == name })
+                return@updateServerStateAndGetValue stateWithoutUnverifiedParticipant to NameVerificationResult.NameAlreadyExists
+            stateWithoutUnverifiedParticipant.copy(
+                participants = stateWithoutUnverifiedParticipant.participants + Participant(name, key)
+            ) to NameVerificationResult.Success
+        }
+
+    override val coroutineContext = coroutineContext
 }
 
 private fun ServerState.participantFor(apiKey: ApiKey) = participants.firstOrNull { it.apiKey == apiKey }
--- server/src/main/kotlin/kmpworkshop/server/ServerState.kt
+++ server/src/main/kotlin/kmpworkshop/server/ServerState.kt
@@ -2,6 +2,7 @@ package kmpworkshop.server
 
 import kmpworkshop.common.ApiKey
 import kmpworkshop.common.getEnvironment
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.Serializable
@@ -15,15 +16,26 @@ internal data class ServerState(
     val unverifiedParticipants: List<Participant>,
 )
 
-private val serverStateLock = Mutex()
-private var serverState by fileBackedProperty<ServerState>(
-    filePath = "/Users/Wout.Werkman/Documents/KotlinMultiplatformWorkshopWarsaw2024/participantsDatabase.csv",
-    defaultValue = { ServerState(emptyList(), emptyList()) },
-)
+internal sealed class WorkshopLevel(val kotlinFile: String) {
+    data object Registration : WorkshopLevel("Registration.kt")
+    data object FirstTask : WorkshopLevel()
+}
 
+internal fun serverState(): Flow<ServerState> = serverStateProperty
 internal suspend fun <T> updateServerStateAndGetValue(update: (ServerState) -> Pair<ServerState, T>): T =
     serverStateLock.withLock {
         val (newState, value) = update(serverState)
         if (newState != serverState) serverState = newState
         value
     }
+
+internal suspend inline fun updateServerState(crossinline update: (ServerState) -> ServerState) {
+    updateServerStateAndGetValue { update(it) to Unit }
+}
+
+private val serverStateLock = Mutex()
+private val serverStateProperty = fileBackedProperty<ServerState>(
+    filePath = "/Users/Wout.Werkman/Documents/KotlinMultiplatformWorkshopWarsaw2024/participantsDatabase.csv",
+    defaultValue = { ServerState(emptyList(), emptyList()) },
+)
+private var serverState by serverStateProperty
