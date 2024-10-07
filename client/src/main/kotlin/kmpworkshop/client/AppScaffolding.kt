e13a5f18d21580fec50acf8c70f9da15c3e32c43
wout.werkman <wout.werkman@jetbrains.com>
2024-10-07 18:48:41 +0100

    Extract Common App logic

---
+++ client/src/main/kotlin/kmpworkshop/client/AppScaffolding.kt
@@ -0,0 +1,18 @@
+@file:Suppress("FunctionName")
+
+package kmpworkshop.client
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+internal fun WorkshopApp(title: String, mainUi: @Composable () -> Unit) {
+    application {
+        Window(onCloseRequest = ::exitApplication, title = title) {
+            MaterialTheme {
+                mainUi()
+            }
+        }
+    }
+}
