78ce53ecc53f21b0b0f23979bf860f5e38f28ccf
brokenhappy <wout_werkman@hotmail.com>
2024-11-11 02:29:01 +0100

    Undo this!

--- client/src/iosMain/kotlin/com/woutwerkman/MainViewController.kt
+++ client/src/iosMain/kotlin/com/woutwerkman/MainViewController.kt
@@ -1,8 +1,24 @@
 package com.woutwerkman
 
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.ComposeUIViewController
-import kmpworkshop.client.SliderGameClient
+import kmpworkshop.client.*
+import kmpworkshop.common.ApiKey
+import kmpworkshop.common.DiscoGameInstruction
+import kmpworkshop.common.asServer
+import kmpworkshop.common.clientApiKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.map
 
 fun MainViewController() = ComposeUIViewController {
-    SliderGameClient()
+    val server = workshopService.asServer(ApiKey(clientApiKey!!))
+    val pressEvents = MutableSharedFlow<Unit>()
+    DiscoGame(object: DiscoGameServer {
+        override fun backgroundColors(): Flow<Color> = server.discoGameBackground().map { it.toComposeColor() }
+        override fun instructions(): Flow<DiscoGameInstruction?> = server.discoGameInstructions(pressEvents)
+        override suspend fun submitGuess() {
+            pressEvents.tryEmit(Unit)
+        }
+    })
 }
