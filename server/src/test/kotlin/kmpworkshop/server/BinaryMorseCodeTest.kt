33d7c79c76eb7972bfe812354abec5975ebe192c
wout.werkman <wout.werkman@jetbrains.com>
2024-11-19 09:29:13 +0100

    Make tests compile again! Let's make them great! Great they shall be!

--- server/src/test/kotlin/kmpworkshop/server/BinaryMorseCodeTest.kt
+++ server/src/test/kotlin/kmpworkshop/server/BinaryMorseCodeTest.kt
@@ -9,7 +9,7 @@ class BinaryMorseCodeTest {
     fun `generates correct ids`() {
         repeat(1_000) {
             val seed = Random.nextLong()
-            val ids = binaryMoreCodeIdentifiers(5, Random(seed))
+            val ids = with(Random(seed)) { binaryMoreCodeIdentifiers(5) }
             assert(ids.size == 5) { "Must yield 5 ids, got $ids, seed = $seed" }
             assert(ids.toSet().toList() == ids) { "Ids must be unique, got $ids, seed = $seed" }
             ids.forEach { id ->
@@ -19,23 +19,29 @@ class BinaryMorseCodeTest {
     }
     @Test
     fun `generates correct length Ids`() {
-        binaryMoreCodeIdentifiers(2).forEach { assertEquals(1, it.length) }
-        binaryMoreCodeIdentifiers(3).forEach { assertEquals(2, it.length) }
-        binaryMoreCodeIdentifiers(4).forEach { assertEquals(2, it.length) }
-        binaryMoreCodeIdentifiers(7).forEach { assertEquals(3, it.length) }
-        binaryMoreCodeIdentifiers(8).forEach { assertEquals(3, it.length) }
-        binaryMoreCodeIdentifiers(9).forEach { assertEquals(4, it.length) }
-        binaryMoreCodeIdentifiers(15).forEach { assertEquals(4, it.length) }
-        binaryMoreCodeIdentifiers(16).forEach { assertEquals(4, it.length) }
-        binaryMoreCodeIdentifiers(17).forEach { assertEquals(5, it.length) }
+        with(Random) {
+            binaryMoreCodeIdentifiers(2).forEach { assertEquals(1, it.length) }
+            binaryMoreCodeIdentifiers(3).forEach { assertEquals(2, it.length) }
+            binaryMoreCodeIdentifiers(4).forEach { assertEquals(2, it.length) }
+            binaryMoreCodeIdentifiers(7).forEach { assertEquals(3, it.length) }
+            binaryMoreCodeIdentifiers(8).forEach { assertEquals(3, it.length) }
+            binaryMoreCodeIdentifiers(9).forEach { assertEquals(4, it.length) }
+            binaryMoreCodeIdentifiers(15).forEach { assertEquals(4, it.length) }
+            binaryMoreCodeIdentifiers(16).forEach { assertEquals(4, it.length) }
+            binaryMoreCodeIdentifiers(17).forEach { assertEquals(5, it.length) }
+        }
     }
 
     @Test fun `count is zero`() {
-        assert(binaryMoreCodeIdentifiers(0).isEmpty())
+        with(Random) {
+            assert(binaryMoreCodeIdentifiers(0).isEmpty())
+        }
     }
 
     @Test fun `count is one`() {
-        assert(binaryMoreCodeIdentifiers(0).isEmpty())
-        assertEquals(1, binaryMoreCodeIdentifiers(1).single().length)
+        with(Random) {
+            assert(binaryMoreCodeIdentifiers(0).isEmpty())
+            assertEquals(1, binaryMoreCodeIdentifiers(1).single().length)
+        }
     }
 }
