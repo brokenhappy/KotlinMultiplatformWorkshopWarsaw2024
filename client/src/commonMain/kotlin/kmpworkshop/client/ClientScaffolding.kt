3a85912ea5c974325c0071d3d27e3717b55bd880
brokenhappy <wout_werkman@hotmail.com>
2024-11-19 17:05:02 +0100

    Fix dummy mistake because I dummy

--- client/src/commonMain/kotlin/kmpworkshop/client/ClientScaffolding.kt
+++ client/src/commonMain/kotlin/kmpworkshop/client/ClientScaffolding.kt
@@ -35,7 +35,7 @@ private fun createService(): WorkshopApiService = runBlocking {
 
     val client: KtorRPCClient = ktorClient.rpc {
         url {
-            host = "localhost"
+            host = "192.168.0.67"
 //            host = "10.0.2.2"
             port = 8080
             encodedPath = WorkshopApiService::class.simpleName!!
