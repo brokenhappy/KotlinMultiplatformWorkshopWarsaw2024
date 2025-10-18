d0e53429cdbf15656ada473e1dfabde1164b0e37
wout.werkman <wout.werkman@jetbrains.com>
2025-10-18 07:10:05 +0200

    Add incorrect sample code for `maximumAgeFindingWithCoroutines`

--- client/src/main/kotlin/kmpworkshop/client/MaximumAgeFindingWithCoroutines.kt
+++ client/src/main/kotlin/kmpworkshop/client/MaximumAgeFindingWithCoroutines.kt
@@ -3,5 +3,5 @@ package kmpworkshop.client
 import kmpworkshop.common.UserDatabase
 
 suspend fun maximumAgeFindingWithCoroutines(database: UserDatabase) {
-
+    database.submit(database.queryUser(database.getAllIds().max()).age)
 }
