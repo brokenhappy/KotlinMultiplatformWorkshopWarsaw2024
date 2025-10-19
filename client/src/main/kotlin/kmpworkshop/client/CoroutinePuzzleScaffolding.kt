77a92606fe7b208f7d5095fce7bb81936d3ded75
wout.werkman <wout.werkman@jetbrains.com>
2025-10-19 05:26:39 +0200

    Fix missing important cleanup scope in puzzles

--- client/src/main/kotlin/kmpworkshop/client/CoroutinePuzzleScaffolding.kt
+++ client/src/main/kotlin/kmpworkshop/client/CoroutinePuzzleScaffolding.kt
@@ -5,16 +5,19 @@ import kmpworkshop.common.CoroutinePuzzleSolutionResult
 import kmpworkshop.common.CoroutinePuzzleSolutionScope
 import kmpworkshop.common.asServer
 import kmpworkshop.common.clientApiKey
+import kmpworkshop.common.withImportantCleanup
 
 suspend fun <T> checkCoroutinePuzzle(
     puzzleId: String,
     solution: suspend (T) -> Unit,
     builder: context(CoroutinePuzzleSolutionScope) () -> T,
 ) {
-    when (val result = workshopService.asServer(ApiKey(clientApiKey!!)).doCoroutinePuzzleSolveAttempt(puzzleId) {
-        solution(builder())
-    }) {
-        is CoroutinePuzzleSolutionResult.Failure -> error(result.description)
-        is CoroutinePuzzleSolutionResult.Success -> println("Yay, you did it!")
+    withImportantCleanup {
+        when (val result = workshopService.asServer(ApiKey(clientApiKey!!)).doCoroutinePuzzleSolveAttempt(puzzleId) {
+            solution(builder())
+        }) {
+            is CoroutinePuzzleSolutionResult.Failure -> error(result.description)
+            is CoroutinePuzzleSolutionResult.Success -> println("Yay, you did it!")
+        }
     }
 }
