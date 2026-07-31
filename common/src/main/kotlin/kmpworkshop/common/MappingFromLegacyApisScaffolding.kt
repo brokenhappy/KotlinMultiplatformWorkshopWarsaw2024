7991bb21328d8ee82b18b14a25cd07057f1a37ef
brokenhappy <wout_werkman@hotmail.com>
2026-07-31 20:33:24 +0200

    Simplify cancellation structure of legacy api mapping

--- common/src/main/kotlin/kmpworkshop/common/MappingFromLegacyApisScaffolding.kt
+++ common/src/main/kotlin/kmpworkshop/common/MappingFromLegacyApisScaffolding.kt
@@ -1,19 +1,13 @@
 package kmpworkshop.common
 
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 context(solutionScope: CoroutinePuzzleSolutionScope)
 suspend fun mapFromLegacyApiWithScaffolding(
     mapFromLegacyApi: suspend (UserDatabaseWithLegacyQueryUser) -> Unit,
 ) {
     withImportantCleanup {
-        flow {
-            emit(1)
-            callLifetime.submitCall(Unit)
-            emit(2)
-        }.collectLatest {
-            if (it == 2) return@collectLatest
+        launch {
             try {
                 mapFromLegacyApi(getUserDatabaseWithLegacyQueryUser(
                     topLevelScope = this@withImportantCleanup,
@@ -27,6 +21,9 @@ suspend fun mapFromLegacyApiWithScaffolding(
                     callIsDone.submitCall(Unit)
                 }
             }
+        }.sideEffect {
+            callLifetime.submitCall(Unit)
+            it.cancel()
         }
     }
 }
