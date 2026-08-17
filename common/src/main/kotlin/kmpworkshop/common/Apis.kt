ff4d0cd25dd53658167f1ae9f526d694fc13f211
brokenhappy <wout_werkman@hotmail.com>
2026-08-17 11:20:39 +0200

    Update to latest public server URL

--- common/src/main/kotlin/kmpworkshop/common/Apis.kt
+++ common/src/main/kotlin/kmpworkshop/common/Apis.kt
@@ -1,7 +1,4 @@
 package kmpworkshop.common
 
-const val serverUrl = "127.0.0.1"
-//const val serverWebsocketPort = 443
-//const val serverUrl = "woutsworkshopv4.europe-west1-gke.intellij.net"
-//const val serverUrl = "kotlinmultiplatformworkshopwarsaw2024.onrender.com"
-const val serverWebsocketPort = 8080
+const val serverUrl = "coroutine-workshop.labs.jb.gg"
+const val serverWebsocketPort = 443
