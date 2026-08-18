a868c929657171f9341437fb36c0096183cb0c06
brokenhappy <wout_werkman@hotmail.com>
2026-08-18 16:06:27 +0200

    Revert changes to NumSumFun.kt Thanks @Emina!

--- workshopSolutions/src/main/kotlin/kmpworkshop/solutions/NumSumFun.kt
+++ workshopSolutions/src/main/kotlin/kmpworkshop/solutions/NumSumFun.kt
@@ -1,14 +1,8 @@
 package kmpworkshop.solutions
 
 import kmpworkshop.api.GetNumberAndSubmit
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 
 suspend fun numberSummer(api: GetNumberAndSubmit) {
-    val sum = coroutineScope {
-        val number1 = async { api.getNumber() }
-        api.getNumber() + number1.await()
-    }
-    api.submit(sum)
+    val number = api.getNumber()
+    api.submit(number + number)
 }
-
